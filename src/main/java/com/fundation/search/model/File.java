c024179920d61c7c60ce5ea1d3080a145f38f380
Mauricio Ramirez <mauricio.ramirez.f@gmail.com>
2018-10-09 10:02:14 -0400

    added file class

---
+++ src/main/java/com/fundation/search/model/File.java
@@ -0,0 +1,21 @@
+/*
+ * @(#)AssetFile.java Copyright (c) 2018 Jalasoft.
+ * 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+ * All rights reserved. This software is the confidential and proprietary information of Jalasoft, ("Confidential Information"). You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement you entered into with Jalasoft.
+ *
+ *
+ */
+
+package com.fundation.search.model;
+
+/**
+ * @autor: Admin
+ * Project: File_Search_B
+ * Package: com.fundation.search.model
+ * Version: 1.0
+ **/
+public class File {
+    public static void main(String[] args){
+        System.out.print("test");
+    }
+}
