d30a6b8fa7773dc8a92c3839df06e0cba539b861
maramirez <bqemauri@us.ibm.com>
2018-10-13 08:50:33 -0400

    first commit from intellij local

--- src/main/java/com/fundation/search/model/File.java
+++ src/main/java/com/fundation/search/model/File.java
@@ -15,7 +15,7 @@ package com.fundation.search.model;
  * Version: 1.0
  **/
 public class File {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.print("test");
     }
 }
