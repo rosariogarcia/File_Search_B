2bc8831e6bf73cc091d29ded2c3fb678f202618f
CristVal <43958880+CristVal@users.noreply.github.com>
2018-10-16 09:49:44 -0400

    Added controller
    Added controller improves

--- src/main/java/com/fundation/search/controller/Controller.java
+++ src/main/java/com/fundation/search/controller/Controller.java
@@ -5,26 +5,44 @@ import com.fundation.search.model.Search;
 import com.fundation.search.model.StorageUnit;
 import com.fundation.search.view.SearchPanel;
 
+import javax.swing.table.DefaultTableModel;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
-    SearchPanel view = new SearchPanel();
-    StorageUnit file = new Folder();
-    StorageUnit folder = new Folder();
+    View view = new View();
     Search search;
 
 
     public Controller() {
-        List<String> searchCriteria = new ArrayList<>();
-        searchCriteria.add(view.getPath());
-        search = new Search(searchCriteria);
-        search.path = view.getPath();
-        search.searchText = view.getFileName();
+        search = new Search(view.getPanel().getPath(), view.getPanel().getFilename());
     }
 
     public void showResults() {
+        String[][] data = {{"Hello", "World"}, {"Hello", "World"}};
+        String[] columns = {"Name", "Type", "Owner", "Create Date", "Modified Date"};
 
+        DefaultTableModel model = new DefaultTableModel(data, columns);
+
+        JScrollPane scrollPane = new JScrollPane(tableResults);
+        tableResults.setFillsViewportHeight(true);
+
+
+        tableResults = new JTable(model);
+        add(tableResults.getTableHeader(), BorderLayout.CENTER);
+        add(tableResults, BorderLayout.CENTER);
+
+        String[][] dataToShow = {{"sdfs", "sda"}, {"asdfsd", "World"}};
+
+        searchButton = new JButton("Search");
+        searchButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                setResults(dataToShow);
+            }
+        });
+
+        DefaultTableModel modelset = (DefaultTableModel) tableResults.getModel();
+        model.addRow(data[0]);
     }
 
     
---
+++ src/main/java/com/fundation/search/controller/Main.java
@@ -0,0 +1,14 @@
+package com.fundation.search.controller;
+
+import javax.swing.table.DefaultTableModel;
+
+public class Main {
+    public static void main(){
+
+        Controller controller = new Controller();
+
+
+
+
+    }
+}
