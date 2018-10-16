588baf3b7823329fd65a4797b72131f9f84a5f0e
CristVal <43958880+CristVal@users.noreply.github.com>
2018-10-16 09:42:15 -0400

    added view fixes
    Added view fixes

--- src/main/java/com/fundation/search/view/SearchPanel.java
+++ src/main/java/com/fundation/search/view/SearchPanel.java
@@ -1,91 +1,57 @@
 package com.fundation.search.view;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.JTableHeader;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class SearchPanel extends JPanel {
 
-    private JPanel singleSearch;
-    private JPanel results;
-    private JPanel advancedSearch;
-
     private JTable tableResults;
-
     private JLabel labelPath;
     private JTextField textFieldPath;
-    private JButton searchButton;
-    private JPanel SearchPanel;
-    private JTextField filename;
     private JLabel labelFilename;
+    private JTextField filename;
+    private JButton searchButton;
 
-    public static void main(String[] args){
-//        Controller icon = new Controller ();
-
-    }
-
-    public SearchPanel(){
+    public SearchPanel() {
         setting();
         init();
+    }
 
+    private void setting() {
 
     }
 
-    private  void setting(){
-
-    }
-    private void init(){
+    private void init() {
         labelPath = new JLabel();
         labelPath.setText("Path:");
 
         textFieldPath = new JTextField();
         textFieldPath.setPreferredSize(new Dimension(350, 25));
-
-        filename = new JTextField();
-        filename.setPreferredSize(new Dimension(300, 25));
         add(labelPath);
         add(textFieldPath);
+
+        labelFilename = new JLabel();
+        labelFilename .setText("Filename:");
         add(labelFilename);
+
+        filename = new JTextField();
+        filename.setPreferredSize(new Dimension(350, 25));
         add(filename);
 
-        String[][] data = {{"Hello", "World"},{"Hello", "World"}};
-        String[] columns = {"Name", "Type", "Owner","Create Date", "Modified Date"};
-
-        DefaultTableModel model = new DefaultTableModel(data, columns);
-
-        JScrollPane scrollPane = new JScrollPane(tableResults);
-        tableResults.setFillsViewportHeight(true);
-
-
-        tableResults = new JTable(model);
-        add(tableResults.getTableHeader(), BorderLayout.CENTER);
-        add(tableResults, BorderLayout.CENTER);
-
-        String[][] dataToShow = {{"sdfs", "sda"},{"asdfsd", "World"}};
-
-        searchButton = new JButton("Search");
-        searchButton.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Tickles!");
-                setResults(dataToShow);
-            }
-        });
-
+        tableResults = new JTable();
+        tableResults.setPreferredSize(new Dimension(350, 25));
+        tableResults.setBackground(Color.WHITE);
         add(tableResults);
+        searchButton = new JButton();
         add(searchButton);
-
     }
 
-    public String getPath(){
+    public String getPath() {
         return textFieldPath.getText();
     }
 
-    public void setResults(String[][] data){
-        DefaultTableModel model = (DefaultTableModel) tableResults.getModel();
-        model.addRow(data);
+    public String getFilename() {
+        return filename.getText();
     }
 
 
--- src/main/java/com/fundation/search/view/View.java
+++ src/main/java/com/fundation/search/view/View.java
@@ -15,15 +15,16 @@ public class View extends JFrame{
         setTitle("Search");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(415,  305);
-
-
     }
 
     private void init (){
         panel = new SearchPanel();
-
         add(panel);
         setVisible(true);
+    }
+
+    public JPanel getPanel(){
+        return panel;
     }
 
     public static void main(String[] args){
